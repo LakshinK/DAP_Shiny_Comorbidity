68f2af0744724266894593e100df1b721f1a4c92
LakshinK <lakshinc02@gmail.com>
2023-02-14 10:27:21 -0800

    Adjusted margins on plot to make it easier to see

--- app.r
+++ app.r
@@ -21,16 +21,27 @@ ui <- fluidPage(
 )
 
 server <- function(input, output){
+  #Reactive expression for pulling which age group was selected
   AgeGroup <- reactive(ageToIndex(input$AgeChosen))
+  #Reactive expression assigning proper Edge List
   EL <- reactive(ELList[[AgeGroup()]] %>%
                    filter(relativeRisk > input$RRCutoff))
+  #Reactive expression assigning proper Vertex List
   VL <- reactive(VLList[[AgeGroup()]] %>%
                    filter(Var1 %in% c(EL()$i, EL()$j)))
+  #Reactive expression creating network
   net <- reactive(graph_from_data_frame(EL(), vertices = VL(), directed = FALSE))
   
+  #Plot output
   output$netPlot <- renderPlot({
-    plot.igraph(net())
-  }, width = 800, height = 800, res = 96)
+    
+    par(mar = c(0,0,0,0))
+    
+    plot(net(),
+                vertex.color = V(net())$type,
+                vertex.label.cex = 0.5,
+                margin = 0)
+  }, width = 1300, height = 1300, res = 300)
 }
 
 shinyApp(ui, server)
