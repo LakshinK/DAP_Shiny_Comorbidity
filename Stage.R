8adba692366f4cb158123c2046a0f55a556d69fe
LakshinK <lakshinc02@gmail.com>
2023-02-14 01:25:54 -0800

    Creation of shiny app template

---
+++ Stage.R
@@ -0,0 +1,33 @@
+yaEL <- youngAdultList[[1]]
+yaVL <- youngAdultList[[2]]
+yaNet <- youngAdultList[[3]]
+yaAdjMat <- youngAdultList[[4]]
+
+miEL <- middleAdultList[[1]]
+miVL <- middleAdultList[[2]]
+miNet <- middleAdultList[[3]]
+miAdjMat <- middleAdultList[[4]]
+
+maEL <- matureAdultList[[1]]
+maVL <- matureAdultList[[2]]
+maNet <- matureAdultList[[3]]
+maAdjMat <- matureAdultList[[4]]
+
+sEL <- seniorList[[1]]
+sVL <- seniorList[[2]]
+sNet <- seniorList[[3]]
+sAdjMat <- seniorList[[4]]
+
+catTable <- data.frame(ageCat = c("Young Adult", "Middle Adult", 
+                                  "Mature Adult", "Senior"),
+                       index = c(1:4))
+
+ELList <- list(yaEL, miEL, maEL, sEL)
+VLList <- list(yaVL, miVL, maVL, sVL)
+
+ageToIndex <- function(AgeCat){
+  catTable %>% 
+    filter(ageCat == AgeCat) %>%
+    pull(index) %>%
+    return()
+}
---
+++ app.r
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(shiny)
+library(igraph)
+
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("DAP Comorbidity"),
+  
+  # Sidebar panel for inputs ----
+  sidebarPanel(
+    #Age Group Selection (access using label AgeGroup)
+    selectInput("AgeChosen", label = "Age Group", c("Young Adult", "Middle Adult", 
+                                           "Mature Adult", "Senior")),
+    #Slider for rel risk cutoff (Access using RRCutoff)
+    numericInput("RRCutoff", "Relative Risk Cutoff", value = 150, min = 0, max = 150)
+  ),
+  
+  # Main panel for displaying outputs ----
+  mainPanel(plotOutput("netPlot"))
+)
+
+server <- function(input, output){
+  AgeGroup <- reactive(ageToIndex(input$AgeChosen))
+  EL <- reactive(ELList[[AgeGroup()]])
+  VL <- reactive(VLList[[AgeGroup()]])
+  net <- reactive(graph_from_data_frame(EL(), vertices = VL(), directed = FALSE))
+  
+  output$netPlot <- renderPlot({
+    plot.igraph(net())
+  }, width = 800, height = 800, res = 96)
+}
+
+shinyApp(ui, server)
